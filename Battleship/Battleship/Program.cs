e15b0f08afc9f0854f7c0defc8064e7f0ba9f08e
Martin R <36480922+gormmmm@users.noreply.github.com>
2026-07-02 16:51:36 +0300

    lesson 5 work

---
+++ Battleship/Battleship/Program.cs
@@ -0,0 +1,146 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        var shipPosition = new Position(2, 1);
+
+        var ship = new Ship(shipPosition, 2); //x123234
+
+        var board = new Board(5, 5, ship);
+
+        var game = new Game();
+
+        game.Play(board);
+
+
+
+        int a = 2;
+        int b = a; // взяли 2 из а и скопировали в b
+        a = 5;
+        //b = 2
+        
+        Position p = new  Position(1, 1);
+        Position p2 = p;
+
+        p.X = 2;
+        
+        //p2.X == 2
+    }
+}
+
+
+class Position
+{
+    public int X { get; set; }
+    public int Y { get; }
+
+    public Position(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
+class Ship
+{
+    // Координаты самой левой верней палубы
+    public Position Position { get; } //null
+    public int Length { get; } //12
+
+    public Ship(Position position, int length)
+    {
+        Position = position;
+        Length = length;
+    }
+}
+
+class Board
+{
+    public int Rows { get; }
+    public int Columns { get; }
+
+    public Ship Ship { get; }
+
+    public Board(int rows, int columns, Ship ship)
+    {
+        Rows = rows;
+        Columns = columns;
+        Ship = ship;
+    }
+
+    public bool IsInside(Position position)
+    {
+        return position.X >= 0 && position.X < Rows && position.Y >= 0 && position.Y < Columns;
+    }
+
+    public bool HasShip(Position position)
+    {
+        return position.Y == Ship.Position.Y && position.X >= Ship.Position.X &&
+               position.X < Ship.Position.X + Ship.Length;
+    }
+}
+
+class Game
+{
+    public void Play(Board board)
+    {
+        var roundCount = 0;
+        while (true)
+        {
+            roundCount++;
+            if (!TryReadFromConsole("X",  roundCount,  out var xPosition))
+                continue;
+
+            Console.WriteLine();
+
+            if (!TryReadFromConsole("Y",  roundCount, out var yPosition))
+                continue;
+
+            var shotPosition = new Position(xPosition, yPosition);
+
+            if (!board.IsInside(shotPosition))
+            {
+                Console.WriteLine("Invalid shot position!");
+                continue;
+            }
+
+            if (board.HasShip(shotPosition))
+            {
+                Console.WriteLine("Hit!");
+            }
+            else
+            {
+                Console.WriteLine("Miss!");
+            }
+        }
+    }
+
+    private bool TryReadFromConsole(string coordinateName, int roundCount, out int coordinate)
+    {
+        Console.WriteLine($"Input your {coordinateName} coordinate for round {roundCount}:");
+        var input = Console.ReadLine();
+        if (!int.TryParse(input, out coordinate))
+        {
+            Console.WriteLine("Invalid input");
+            return false;
+        }
+        
+        return true;
+    }
+}
+
+// Ship
+// Board 
+// Position
+// Game
+
+// Rows = 5
+
+// 0 1 2 3 4 
+// ------------X
+// X X X X X 
+// X X S S X   
+// X X X X X 
+// X X X X X 
+// X X X X X 
+// Y
