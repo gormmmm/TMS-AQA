827e71b7cdf2041c43d420411b432c4d94049ba4
erien1410 <aquedoc@gmail.com>
2026-07-06 23:22:25 +0200

    Homework 3

--- RPS/TMS-1/GameResult.cs
+++ RPS/TMS-1/GameResult.cs
@@ -2,5 +2,21 @@
 
 public class GameResult
 {
+    public Move PlayerMove { get; }
+    public Move ComputerMove { get; }
+    public string ResultText { get; }
     
+    public GameResult(Move playerMove, Move computerMove, string resultText)
+    {
+        PlayerMove = playerMove;
+        ComputerMove = computerMove;
+        ResultText = resultText;
+    }
+    
+    public void Print()
+    {
+        Console.WriteLine($"You chose {PlayerMove.Name}");
+        Console.WriteLine($"Computer chose {ComputerMove.Name}");
+        Console.WriteLine($"--- {ResultText} ---");
+    }
 }
--- RPS/TMS-1/Move.cs
+++ RPS/TMS-1/Move.cs
@@ -8,4 +8,48 @@ public class Move
     {
         _moveValue = moveValue;
     }
+    
+    public int Number => _moveValue;
+    
+    public string Name
+    {
+        get
+        {
+            switch (_moveValue)
+            {
+                case 1: return "Rock";
+                case 2: return "Paper";
+                case 3: return "Scissors";
+                case 4: return "Well";
+                default: return "Unknown";
+            }
+        }
+    }
+    
+    public bool IsValid()
+    {
+        return _moveValue >= 1 && _moveValue <= 3;
+    }
+    
+    public static Move ReadFromConsole()
+    {
+        string userInput = Console.ReadLine();
+        
+        if (int.TryParse(userInput, out int choice))
+        {
+            return new Move(choice);
+        }
+        
+        return new Move(-1);
+    }
+    
+    public static Move GenerateRandom()
+    {
+        var random = new Random();
+        int choice = random.Next(1, 4);
+        
+        return new Move(choice);
+    }
 }
+
+
