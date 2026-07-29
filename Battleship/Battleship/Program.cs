303632fc89816dc164f0c84392ca42523607c22c
erien1410 <aquedoc@gmail.com>
2026-07-29 16:58:05 +0200

    Домашнее задание 5-6

--- Battleship/Battleship/Program.cs
+++ Battleship/Battleship/Program.cs
@@ -82,37 +82,96 @@ class Board
 
 class Game
 {
+    private readonly Random _random = new Random();
+    
+    public int PlayerHits { get; private set; }
+    public int ComputerHits { get; private set; }
     public void Play(Board board)
     {
+        var opponentBoard = GenerateOpponentBoard(board.Rows, board.Columns);
         var roundCount = 0;
+        
+        //счетчики
+        PlayerHits = 0;
+        ComputerHits = 0;
+        
         while (true)
         {
             roundCount++;
-            if (!TryReadFromConsole("X",  roundCount,  out var xPosition))
-                continue;
+            // Ходит игрок
+            var shotPosition = GetValidPlayerShot(opponentBoard, roundCount);
 
-            Console.WriteLine();
-
-            if (!TryReadFromConsole("Y",  roundCount, out var yPosition))
-                continue;
-
-            var shotPosition = new Position(xPosition, yPosition);
-
-            if (!board.IsInside(shotPosition))
-            {
-                Console.WriteLine("Invalid shot position!");
-                continue;
-            }
-
-            if (board.HasShip(shotPosition))
+            if (opponentBoard.HasShip(shotPosition))
             {
                 Console.WriteLine("Hit!");
+                PlayerHits++;
             }
             else
             {
                 Console.WriteLine("Miss!");
             }
+
+            // Ходит компьютер
+            MakeComputerMove(board);
+
+            // Выводим счет
+            Console.WriteLine($"\n--- End of Round {roundCount} ---");
+            Console.WriteLine($"Score -> Player: {PlayerHits} | Computer: {ComputerHits}\n");
         }
+    }
+    
+    private Position GetValidPlayerShot(Board targetBoard, int roundCount)
+    {
+        while (true)
+        {
+            if (!TryReadFromConsole("X", roundCount, out var xPosition))
+                continue;
+
+            Console.WriteLine();
+
+            if (!TryReadFromConsole("Y", roundCount, out var yPosition))
+                continue;
+
+            var shotPosition = new Position(xPosition, yPosition);
+
+            if (!targetBoard.IsInside(shotPosition))
+            {
+                Console.WriteLine("Invalid shot position! Coordinates are out of board limits. Try again.\n");
+                continue; 
+            }
+
+            return shotPosition; // Возвращаем позицию только если она полностью корректна
+        }
+    }
+    
+    private void MakeComputerMove(Board playerBoard)
+    {
+        int compX = _random.Next(0, playerBoard.Columns);
+        int compY = _random.Next(0, playerBoard.Rows);
+        var compShotPosition = new Position(compX, compY);
+
+        Console.WriteLine($"Computer shoots at X: {compX}, Y: {compY}");
+
+        if (playerBoard.HasShip(compShotPosition))
+        {
+            Console.WriteLine("Computer Hit!");
+            ComputerHits++;
+        }
+        else
+        {
+            Console.WriteLine("Computer Miss!");
+        }
+    }
+    private Board GenerateOpponentBoard(int rows, int columns)
+    {
+        int length = _random.Next(1, columns + 1); 
+        int y = _random.Next(0, rows);
+        int x = _random.Next(0, columns - length + 1);
+        
+        var shipPosition = new Position(x, y);
+        var ship = new Ship(shipPosition, length);
+        
+        return new Board(rows, columns, ship);
     }
 
     private bool TryReadFromConsole(string coordinateName, int roundCount, out int coordinate)
